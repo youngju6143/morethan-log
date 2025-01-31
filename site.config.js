3083f3af18f02a0d9239f8a12f794c45360307ca
youngju6143 <youngju6143@naver.com>
2025-01-31 19:03:19 +0900

    ✨ Activate Utterance comment

--- site.config.js
+++ site.config.js
@@ -56,7 +56,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "youngju6143/morethan-log",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
