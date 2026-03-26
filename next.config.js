c7fe79b27c7b722524949ecaaa83e5438a5f0332
youngju6143 <youngju6143@naver.com>
2026-03-26 19:36:31 +0900

    ✨ feat: next.config.js에 이미지 도메인 추가

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   images: {
     domains: [
+      "notion.so",
       "www.notion.so",
       "lh5.googleusercontent.com",
+      "s3.us-west-2.amazonaws.com",
       "s3-us-west-2.amazonaws.com",
       "prod-files-secure.s3.us-west-2.amazonaws.com",
     ],
